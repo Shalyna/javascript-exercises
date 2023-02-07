8d08efb144c649e5686dfdb282dfe2697da63c38
“SamR248” <r.shalyna@gmail.com>
2023-02-07 15:01:20 -0800

    finished Assig.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,13 @@
 
 
 
-const repeatString = function(word, times) {
-    if (times < 0) return 'ERROR';
-    let string = '';
-    for (let i = 0; i < times; i++) {
-      string += word;
+  
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
     }
-    return string;
-  };
+    return string.repeat(num);
+}
+
 // Do not edit below this line
 module.exports = repeatString;
